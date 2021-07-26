317ded783277bb4683e99e654e967f40d716a627
gerryhzgtest2021 <85722845+gerryhzgtest2021@users.noreply.github.com>
2021-07-26 15:30:11 -0700

    Add files via upload

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'wordpress_db' );
+define( 'DB_NAME', 'mysql' );
 
 /** MySQL database username */
-define( 'DB_USER', 'wordpress_user' );
+define( 'DB_USER', 'root' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'your_strong_password' );
+define( 'DB_PASSWORD', 'tempunsecurepassword' );
 
 /** MySQL hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'conenction string of your RDS' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
